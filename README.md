6300019ab2dcc5dcb90b241fb55847a8c8b95805
thetaversefarm <104188318+thetaversefarm@users.noreply.github.com>
2022-04-24 07:20:35 +0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# thetaverse-token-list
- 
+
+## Adding a token
+
+To request that we add a token to the list, 
+[file an issue](https://github.com/thetaversefarm/thetaverse-token-list/issues/new?assignees=&labels=token+request&template=token-request.md&title=Add+%7BTOKEN_SYMBOL%7D%3A+%7BTOKEN_NAME%7D).
+
+### Disclaimer
+
+Note filing an issue does not guarantee addition to this default token list.
+We do not review token addition requests in any particular order, and we do not
+guarantee that we will review your request to add the token to the default list.
+
---
+++ src/buildList.js
@@ -0,0 +1,33 @@
+const { version } = require('../package.json');
+const mainnet = require('./tokens/mainnet.json');
+const testnet = require('./tokens/testnet.json');
+
+module.exports = function buildList() {
+  const parsed = version.split('.');
+  return {
+    'name': 'Thetaverse List',
+    'timestamp': (new Date().toISOString()),
+    'version': {
+      'major': +parsed[ 0 ],
+      'minor': +parsed[ 1 ],
+      'patch': +parsed[ 2 ]
+    },
+    'tags': {},
+    'logoURI': 'https://raw.githubusercontent.com/thetaversefarm/thetaverse-token-list/master/images/logo.png',
+    'keywords': [
+      'thetaverse',
+      'default'
+    ],
+    tokens: [
+      ...mainnet,
+      ...testnet,
+    ]
+      // sort them by symbol for easy readability
+      .sort((t1, t2) => {
+        if (t1.chainId === t2.chainId) {
+          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+        }
+        return t1.chainId < t2.chainId ? -1 : 1;
+      })
+  };
+};
